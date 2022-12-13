8a52ac6259c701489d7d144b0d4cabb445434eb9
daniel jackson <jackdee0077@gmail.com>
2022-12-12 21:54:01 -0500

    to-do-app

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,6 +1,16 @@
 from django.forms import ModelForm
-from todo_app.models import Task
+from todo_app.models import Task, Comment
+
 class TaskForm(ModelForm):
-    class Meta:
+    class Meta():
         model = Task
         fields = ['description']
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body'] 
+    def __init__(self, *args, **kwargs):
+        task_object = kwargs.pop('task')
+        super().__init__(*args, **kwargs)
+        self.instance.task = task_object
--- todo_app/views.py
+++ todo_app/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render,redirect
 # Create your views here.
 
 from django.views import View
-from todo_app.models import Task
-from todo_app.forms import TaskForm
+
+from todo_app.models import Task, Comment
+from todo_app.forms import TaskForm, CommentForm
+
 
 class HomeView(View): 
+    '''
+        Home view function as the site homepage, 
+        listing out all task objects in data base and linking out each ones detail view
+        '''
     def get(self, request):
+        '''
+        the content requires to render the homepage
+        '''
+
         task_form = TaskForm()
         task = Task.objects.all()
         html_data = {
@@ -17,24 +27,38 @@ class HomeView(View):
         return render(
             request=request,
             template_name='index.html',
-            context={
-            'task_list' : task,
-            'form' : task_form,
-        }
+            context= html_data
+            
         )
     def post(self, request):
-        print(request.POST)
+        '''
+        this method saves new TAsks to the data base before redirecting'''
         task_form = TaskForm(request.POST)
         task_form.save()
 
         return redirect('home')
+
 class TaskDetailView(View):
+    '''
+    TaskDetailView provides the ability to update and delete indidual 
+    Task object from the database 
+    '''
     def get(self, request, task_id):
+        ''' 
+        The content required to render a task object's detail pagepy
+        '''
         task = Task.objects.get(id=task_id)
         task_form = TaskForm(instance=task)
+
+
+        comments = Comment.objects.filter(task=task)
+        comment_form = CommentForm(task=task)
+
         html_data = {
             'task_object' : task,
             'form' : task_form,
+            'comment_list': comments,
+            'comment_form': comment_form
         }
         return render(
             request=request,
@@ -48,15 +72,14 @@ class TaskDetailView(View):
             task_form.save()
         elif 'delete' in request.POST:
             task.delete()
+        elif 'add' in request.POST:
+            comment_form = CommentForm (request.POST, task=task)
+            comment_form.save()
+            return redirect('task_detail', task_id=task_id)
         return redirect ('home')
 
-        # tasks = Task.objects.all()
-        # html_data = {
-        #     'task_list' : tasks,
-        #     'form': task_form,
-        # }
-        # return render(
-        #     request=request,
-        #     template_name='index.html',
-        #     context=html_data,
-        # )
+
+
+
+
+        
